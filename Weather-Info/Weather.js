e041c444e758984d6508d7a5f7fa973dca0e6723
darshjnn <darshjain12345654321@gmail.com>
2023-11-26 22:54:24 +0530

    Added Functionality to search for cuurent location

--- Weather-Info/Weather.js
+++ Weather-Info/Weather.js
@@ -15,11 +15,16 @@ function getLocation() {
     alert(position)
 }
 
-//FOR CURRENT LOCATION
+var contain = document.getElementById("container");
 var img = document.getElementById("weathImg");
 var desc = document.getElementById("description");
+var temp = document.getElementById("temp");
+var inpLoc = document.getElementById("location");
+var city = document.getElementById("city");
+var humidity = document.getElementById("humid");
+var speed = document.getElementById("speed");
 
-
+//FOR CURRENT LOCATION
 function myLoc() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -33,15 +38,52 @@ function myLoc() {
             fetch(base).then((response) => {
                 return response.json();
             }).then((data) => {
-                const { temp } = data.main;
-                const place = data.name;
+                temp.textContent = data.main.temp + "°C";
+                inpLoc.value = data.name;
+                city.textContent = data.name;
                 desc.textContent = data.weather[0].description;
+                // desc.textContent = "clear";
                 desc.textContent = desc.textContent.charAt(0).toUpperCase() + desc.textContent.slice(1);
-                if(desc.textContent.includes("aze")){
-                    img.src = '/Front-end-Mini-Projects/Weather-Info/Images/rain.png'
-                }
+                humidity.textContent = data.main.humidity + "%";
+                speed.textContent = data.wind.speed + " Km/hr";
+                getImgBg();
             })
         })
     }
 };
 
+//CHANGE BACKGROUND & IMAGE FUNCTION 
+function getImgBg() {
+    if (desc.textContent.includes("cast")) {
+        contain.style.background = 'linear-gradient(-45deg, rgba(198, 151, 73, 0.6), rgba(40, 42, 58, 0.6))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/overcast-clouds.png';
+    } else if (desc.textContent.includes("lear")) {
+        contain.style.background = 'linear-gradient(-45deg, rgba(255, 214, 165, 1), rgba(255, 254, 196, 1))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/clear.png';
+        contain.style.color = "rgba(35, 45, 63, 1)"
+    } else if (desc.textContent.includes("oud")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/cloud.png';
+    } else if (desc.textContent.includes("izzle")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/drizzle.png';
+    } else if (desc.textContent.includes("hower")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/drizzle.png';
+    } else if (desc.textContent.includes("ain")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/rain.png';
+    } else if (desc.textContent.includes("torm")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/thunder.png';
+    } else if (desc.textContent.includes("now")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/snow.png';
+    } else if (desc.textContent.includes("ist")) {
+        contain.style.background = 'linear-gradient(-45deg, rgb(247, 140, 162), rgb(147, 140, 162))';
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/mist.png';
+    } else {
+        contain.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+        img.src = '/Front-end-Mini-Projects/Weather-Info/Images/noValue.png';
+    }
+}
