5359bf73cd2d4c9ee95c6e48cc3fc9def168e17f
darshjnn <darshjain12345654321@gmail.com>
2023-12-03 21:46:26 +0530

    Added To-Do-List App

---
+++ To-Do-List/README.md
@@ -0,0 +1,3 @@
+# To Do List
+It is a to do list app developed using HTML, CSS, JavaScript. <br>
+Author: Darsh Jain
---
+++ To-Do-List/ToDo.js
@@ -0,0 +1,40 @@
+const inputText = document.getElementById("textCont");
+const inputBtn = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+function addTask() {
+    if (inputText.value === ' ' || inputText.value === '') {
+        alert('Write Something to add a task...')
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputText.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputText.value = "";
+    saveData();
+}
+
+list.addEventListener("click", function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+//TO SAVE DATA
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+//TO DISPLAY VALUE
+function showTask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
