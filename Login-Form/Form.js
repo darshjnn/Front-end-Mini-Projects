38b18fdcd4092eedf55c4892768dd6f739d981f6
darshjnn <darshjain12345654321@gmail.com>
2024-02-24 16:54:32 +0530

    Added features in Form

--- Login-Form/Form.js
+++ Login-Form/Form.js
@@ -22,13 +22,13 @@ userDobInp.onblur = (e) => {
     }
 }
 
-// For Log in Button Hover Animation
+// For All Button's Hover Animation
 const hoverFunction = document.getElementsByClassName("hoverFunction");
 for (let i = 0; i < hoverFunction.length; i++) {
     hoverFunction[i].onmousemove = function (e) {
         const x = e.layerX;
         const y = e.layerY;
-        
+
         hoverFunction[i].style.setProperty('--x', x + 'px');
         hoverFunction[i].style.setProperty('--y', y + 'px');
     }
@@ -38,7 +38,7 @@ for (let i = 0; i < hoverFunction.length; i++) {
 const userPswInp = document.getElementById("userPswInp");
 const generatePswBtn = document.getElementById("generatePswBtn");
 
-const length = 10;
+const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -85,41 +85,193 @@ document.getElementById("copyBtn").addEventListener('click', () => {
 const copyBtn = document.getElementById("copyBtn");
 const copyBtnImg = document.getElementById("copyBtnImg");
 const resetBtn = document.getElementById("resetBtn");
+const copyBtnHoverContent = document.getElementById("copyBtnHoverContent");
+const copyBtnHover = document.getElementById("copyBtnHover");
 
 copyBtn.addEventListener('click', () => {
     copyBtnImg.src = '/Front-end-Mini-Projects/Login-Form/SVG/check-square-svgrepo-com.svg';
-    setTimeout(resetCopyImg, 3000);
+    copyBtnHoverContent.innerHTML = "Copied!";
+    copyBtnHover.classList.add("copiedBtnHover");
+    setTimeout(resetCopyBtn, 3000);
 });
 
 resetBtn.addEventListener('click', () => {
     copyBtnImg.src = '/Front-end-Mini-Projects/Login-Form/SVG/copy-svgrepo-com.svg';
+    copyBtnHoverContent.innerHTML = "Copy?";
+    copyBtnHover.classList.remove("copiedBtnHover");
 });
 
 generatePswBtn.addEventListener('click', () => {
     copyBtnImg.src = '/Front-end-Mini-Projects/Login-Form/SVG/copy-svgrepo-com.svg';
+    copyBtnHoverContent.innerHTML = "Copy?";
+    copyBtnHover.classList.remove("copiedBtnHover");
 });
 
-function resetCopyImg() {
+function resetCopyBtn() {
     copyBtnImg.src = '/Front-end-Mini-Projects/Login-Form/SVG/copy-svgrepo-com.svg';
+    copyBtnHoverContent.innerHTML = "Copy?";
+    copyBtnHover.classList.remove("copiedBtnHover");
 }
 
 // To Hide-Show Password
 const PswInp = document.getElementsByClassName("PswInp");
 const passwordEye = document.getElementsByClassName("passwordEye");
 const showPassword = document.getElementsByClassName("showPassword");
+const animateHeight = document.getElementsByClassName("animateHeight");
+const passwordEyeHoverContent = document.getElementsByClassName("passwordEyeHoverContent");
 
-for (let i = 0; i < showPassword.length; i++) {
+for (let i = 0; i < passwordEye.length; i++) {
     passwordEye[i].addEventListener('click', () => {
-        if (showPassword[i].style.display == "block") {
-            showPassword[i].style.display = "none";
+        if (passwordEye[i].className.includes('showPassword')) {
+            passwordEye[i].classList.remove('showPassword');
             PswInp[i].type = "text";
+            animateHeight[i].style.height = "0";
+            animateHeight[i].style.bottom = "3px";
+            animateHeight[i].style.top = "auto";
+            passwordEyeHoverContent[i].innerHTML = "Hide";
         }
-        else if (showPassword[i].style.display = "none") {
-            showPassword[i].style.display = "block";
+        else {
+            passwordEye[i].classList.add('showPassword');
             PswInp[i].type = "password";
+            animateHeight[i].style.height = "35px";
+            animateHeight[i].style.top = "6px";
+            animateHeight[i].style.bottom = "auto";
+            passwordEyeHoverContent[i].innerHTML = "Show";
         }
     });
 }
 
-// Password Strength Checker
+// Form Validation
+/*
+Check following website for form validation:
+https://www.w3resource.com/javascript/form/javascript-form-validation.php
+*/
+
+const submitBtn = document.querySelectorAll('button[type = "submit"]');
+const createAccBtn = document.getElementById("createAccBtn");
+
+// 1. Check Empty Fields
+// Check if there is any empty field left and show a prompt if any empty field is there
+const userloginInfo = document.getElementsByClassName('userloginInfo');
+const loginField = document.getElementsByClassName("loginField");
+
+const newAccInfo = document.getElementsByClassName('newAccInfo');
+const createAccField = document.getElementsByClassName("createAccField");
+
+for (let i = 0; i < submitBtn.length; i++) {
+    submitBtn[i].addEventListener('click', () => {
+        if (submitBtn[i].id == "logInBtn") {
+            for (let j = 0; j < userloginInfo.length; j++) {
+                checkEmptyField(userloginInfo[j], loginField[j]);
+            }
+        }
+        else if (submitBtn[i].id == "createAccBtn") {
+            for (let j = 0; j < newAccInfo.length; j++) {
+                checkEmptyField(newAccInfo[j], createAccField[j]);
+            }
+        }
+    });
+}
+
+function checkEmptyField(e, f) {
+    if (e.value == "") {
+        f.classList.add('emptyFieldPopup');
+        setTimeout(() => {
+            f.classList.remove('emptyFieldPopup');
+        }, 5000);
+    }
+    else {
+        f.classList.remove('emptyFieldPopup');
+    }
+}
+
+// Preventing Default pop-up of "required" input field without removing validation
+const requiredInput = document.querySelectorAll('input');
+for (let i = 0; i < requiredInput.length; i++) {
+    document.addEventListener('invalid', (function () {
+        return function (e) {
+            e.preventDefault();
+            requiredInput[i].focus();
+        };
+    })(), true);
+}
+
+// 2. Name field Validation for letters only
+// Verify if the user has entered only alphabets in Enter Name input
+let letters = /^[A-za-z]+$/;
+const userNameInp = document.NewUserRegistration.userNameInp;
+createAccBtn.addEventListener('click', () => {
+    if (userNameInp.value.match(letters)) {
+        return true;
+    }
+    else {
+        userNameInp.parentElement.classList.add("invalidFieldPopup");
+        setTimeout(() => {
+            userNameInp.parentElement.classList.remove("invalidFieldPopup");
+        }, 5000);
+    }
+});
+
+// 3. E-mail Validation
+
+/*
+Regular Expression Pattern for E-mail Validation
+let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+*/
+
+// RFC 2822 Standard E-mail Validation
+let mailFormat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]| \\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?| \[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]: (?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+const userMailInp = document.NewUserRegistration.userMailInp;
+createAccBtn.addEventListener('click', () => {
+    if (userMailInp.value.match(mailFormat)) {
+        return true;
+    }
+    else {
+        userMailInp.parentElement.classList.add("invalidFieldPopup");
+        setTimeout(() => {
+            userMailInp.parentElement.classList.remove("invalidFieldPopup");
+        }, 5000);
+    }
+});
+
+// 4. Mobile Number Validation
+/*
+To valididate a phone number like:
+XXX-XXX-XXXX
+XXX.XXX.XXXX
+XXX XXX XXXX
+*/
+let numberFormat = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+const userMobileInp = document.getElementById("userMobileInp");
+createAccBtn.addEventListener('click', () => {
+    if (userMobileInp.value.match(numberFormat)) {
+        return true;
+    }
+    else {
+        userMobileInp.parentElement.classList.add("invalidFieldPopup");
+        setTimeout(() => {
+            userMobileInp.parentElement.classList.remove("invalidFieldPopup");
+        }, 5000);
+    }
+});
+
+// 5. Password Validation & Password Strength Checker
+
 // https://www.w3resource.com/javascript/form/javascript-form-validation.php
+const strongPsw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{12,15}$/;
+const mediumPsw1 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,11}$/;
+const mediumPsw2 = /^(?=.*\d)(?=.*[A-Z])(?!.*\s).{5,8}$/;
+const mediumPsw3 = /^(?=.*\d)(?=.*[a-z])(?!.*\s).{5,8}$/;
+
+createAccBtn.addEventListener('click', () => {
+    if (userPswInp.value.match(strongPsw)) {
+        console.log("kadak h");
+    }
+    else if (userPswInp.value.match(mediumPsw1) || userPswInp.value.match(mediumPsw2) || userPswInp.value.match(mediumPsw3)) {
+        console.log("thik hi h")
+    }
+    else {
+        console.log("katega")
+    }
+});
--- Login-Form/README.md
+++ Login-Form/README.md
@@ -1,3 +1,12 @@
 # Login Form
 This is a Responsive Login form created using HTML,CSS, JavaScript. <br>
+Features: <br>
+1. Rotaing Card
+2. Direction Aware Hover Buttons
+3. Animated Show/Hide Password Eye
+4. Form Validation with Name, E-mail & Mobile Validation
+5. Password Strength Checker
+6. Generate Strong Password
+7. Password  Copy Button
+
 Author: Darsh Jain
